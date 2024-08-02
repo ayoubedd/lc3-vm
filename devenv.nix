0888487f4bf4ca8484598bd2b7e62c440d9d5406
Ayoub Eddaoudi <me@ayoubedd.me>
2024-08-02 21:07:08 +0100

    refactor(devenv.nix): to easily add more tests in the future

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,12 @@
 { pkgs, lib, config, inputs, ... }:
-
+let 
+  files = [ "rogue" "2028" "hello-world" ];
+  runner = file: "lc3as ./examples/${file} && cargo run -- ./examples/${builtins.baseNameOf file}.obj";
+  scripts = builtins.listToAttrs (map (file: { name = file; value = { exec = runner file; }; }) files);
+in
 {
+  inherit scripts;
   packages = with pkgs; [ lc3tools ];
-
-  scripts.rc.exec = "lc3as examples/hello-world.asm && cargo run -- examples/hello-world.obj";
 
   languages.rust = {
     enable = true;
